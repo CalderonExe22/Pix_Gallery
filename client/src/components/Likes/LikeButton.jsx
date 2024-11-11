1bdf2ba7a53a22997e7f351280f1a1177a6e0633
JonathanAlveal <jonathan.alveal@est.fi.uncoma.edu.ar>
2024-11-11 18:29:14 -0300

    Update Interactions

--- client/src/components/Likes/LikeButton.jsx
+++ client/src/components/Likes/LikeButton.jsx
@@ -4,28 +4,41 @@ import PropTypes from 'prop-types';
 
 export default function LikeButton({ photoId }) {
     const [likes, setLikes] = useState([]);
+    const [likesPhoto, setLikesPhoto] = useState(0);
     const [isInLikes, setIsInLikes] = useState(false);
 
-    const getLikes = async () => {
+    const getLikesUser = async () => {
         try {
             const response = await axiosApi.get('likes/likes/get_user_likes/');
             setLikes(response.data);
-            console.log(response.data);
+            //console.log(response.data);
             setIsInLikes(response.data.some(photo => photo.photo === photoId));
         } catch (error) {
             console.error('Error al obtener los Likes:', error);
         }
     };
+
+    const getLikesPhoto = async () => {
+        try {
+            const likes = await axiosApi.get('likes/likes/get_all_likes/');
+            //console.log(likes.data.filter(like => like.photo === parseInt(photoId)))
+            setLikesPhoto((likes.data.filter(like => like.photo === parseInt(photoId))).length)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
     useEffect(() => {
-        getLikes();
+        getLikesUser();
+        getLikesPhoto();
     }, []);
 
     const handleAddToLike = async () => {
         try {
             const response = await axiosApi.post('likes/likes/', { photo: photoId });
             console.log(response.data);
-            getLikes();
+            getLikesUser();
+            getLikesPhoto();
         } catch (error) {
             console.log('Error al dar Like:', error);
         }
@@ -36,7 +49,8 @@ export default function LikeButton({ photoId }) {
             const photo_id = likes.find(photo => photo.photo === photoId).id;
             const response = await axiosApi.delete(`/likes/likes/${photo_id}/`);
             console.log(response);
-            getLikes();
+            getLikesUser();
+            getLikesPhoto();
         } catch (error) {
             console.log('Error al quitar Like:', error);
         }
@@ -45,13 +59,13 @@ export default function LikeButton({ photoId }) {
     return (
         <div>
             <button 
-                className="text-red-500 bg-white text-3xl h-12 w-12 rounded-full hover:text-white hover:bg-red-500" 
+                className="text-red-500 bg-white text-3xl h-12 w-20 rounded-full hover:text-white hover:bg-red-500" 
                 onClick={() => isInLikes ? handleRemoveToLike() : handleAddToLike()}>
                 {
                     isInLikes ? 
-                        <i className="fa-sharp fa-solid fa-heart"></i>
+                        <i className="fa-sharp fa-solid fa-heart"><span className='ml-2'>{likesPhoto}</span></i>
                     : 
-                        <i className="fa-sharp fa-regular fa-heart"></i>
+                        <i className="fa-sharp fa-regular fa-heart"><span className='ml-2'>{likesPhoto}</span></i>
                 }
             </button>
         </div>
